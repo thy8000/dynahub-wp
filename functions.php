50158fa34375eee01d95dbf74f878385e862f6d4
Thunay Moreira de Soares <thunay@buildbox.com.br>
2026-05-26 22:14:38 -0300

    🚧 Header

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+if(!defined('ABSPATH')) {
+    exit;
+}
+
+add_action('wp_enqueue_scripts', 'dynahub_enqueue_scripts');
+function dynahub_enqueue_scripts() {
+    wp_enqueue_style('dynahub-style', get_template_directory_uri() . '/assets/css/style.css');
+}
